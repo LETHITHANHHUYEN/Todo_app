96244909721d76dec5dd1d0980419f26a9bd0ade
LETHITHANHHUYEN <huyenltt098@gmail.com>
2022-03-30 16:00:09 +0700

    finaliy commit

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
         <div style={{ textAlign: 'left' }}>
             <h3>Made by 4501104098 - LE THI THANH HUYEN</h3>
             <h3>
-                Github: <a href="https://github.com/LETHITHANHHUYEN">here</a>
+                Github: <a href="https://github.com/LETHITHANHHUYEN/todo_app">here</a>
             </h3>
         </div>
     )
